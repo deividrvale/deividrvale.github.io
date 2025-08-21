ee38eebee70216d992a2c1323c9f0bec56155fa0
deividrvale <deividrodriguesvale@gmail.com>
2025-08-21 05:52:12 +0000

    Deploying to gh-pages from @ deividrvale/deividrvale.github.io@d923d2bfc135f3a5af0e85acbbfaafb4233f3b72 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,18 +387,17 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-starting-a-postdoctoral-research-fellow-position-at-augusta-university",
+          title: 'Starting a Postdoctoral Research Fellow Position at Augusta University',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+              window.location.href = "/news/announcement_0/";
+            },},{id: "news-journal-paper-accepted-at-the-logical-methods-in-computer-science",
+          title: 'Journal paper accepted at the Logical Methods in Computer Science',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
